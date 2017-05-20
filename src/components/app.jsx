d984faf04205e7fc99cab3856df827cf2fee9a04
Jonathan Chiam <jczjchiam@gmail.com>
2017-05-20 23:36:27 +0800

    Implement header bar (#4)

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import FlatButton from 'material-ui/FlatButton';
+import { red100 } from 'material-ui/styles/colors';
+
+const headerStates = {
+  HOME: 'home',
+  TIMELINE: 'timeline'
+};
+
+const styles = {
+  app: {
+    margin: -8
+  },
+  headerBar: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: 8,
+    height: 50,
+    background: red100
+  },
+  headerButton: {
+    height: 50,
+    border: 'none',
+    margin: '0 10px'
+  },
+  headerButtonSelected: {
+    height: 50,
+    borderBottom: '5px solid red',
+    margin: '0 10px'
+  }
+};
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headerState: headerStates.HOME
+    };
+  }
+
+  renderHeaderBar() {
+    const { headerState } = this.state;
+    return (
+      <div style={styles.headerBar}>
+        <FlatButton
+          style={headerState === headerStates.HOME ? styles.headerButtonSelected : styles.headerButton}
+          label="Home"
+          onClick={() => this.setState({ headerState: headerStates.HOME })}
+          secondary
+        />
+        <FlatButton
+          style={headerState === headerStates.TIMELINE ? styles.headerButtonSelected : styles.headerButton}
+          label="Timeline"
+          onClick={() => this.setState({ headerState: headerStates.TIMELINE })}
+          secondary
+        />
+      </div>
+    );
+  }
+
   render() {
-    return <div>{this.props.children}</div>;
+    return (
+      <div style={styles.app}>
+        {this.renderHeaderBar()}
+        {this.props.children}
+      </div>
+    );
   }
 }
 
